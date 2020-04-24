d99ba088902979dc793067a407e8cc8b88d2515d
Francisco de Castro <franciscoescastro@gmail.com>
2020-04-23 18:21:45 -0600

    Implement routes file
    - Add suport to routes
    
    Add global css
    
    - Implement global css
    - Add assets

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
 import React from 'react'
+import { BrowserRouter } from 'react-router-dom'
+import GlobalStyles from './styles/global'
+import Routes from './routes'
 
-function App() {
-  return <h1>Hello Chico</h1>
-}
+const App: React.FC = () => (
+  <>
+    <BrowserRouter>
+      <Routes />
+    </BrowserRouter>
+    <GlobalStyles />
+  </>
+)
 
 export default App
