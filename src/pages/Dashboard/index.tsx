ae3c01a90b0c287016a412e9d81d6d6c4fcb295d
Francisco de Castro <franciscoescastro@gmail.com>
2020-04-26 15:05:45 -0600

    Implement repository page layout

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,8 +1,35 @@
 import React from 'react'
-import { Title } from './styles'
+import { FiChevronRight } from 'react-icons/fi'
+import { Title, Form, Repositories } from './styles'
+import logoImg from '../../assets/logo.svg'
 
 const Dashboard: React.FC = () => {
-  return <Title>Explore Github repositories</Title>
+  return (
+    <>
+      <img src={logoImg} alt="Github explorer logo" />
+      <Title>Explore Github repositories</Title>
+
+      <Form>
+        <input placeholder="Find a repository" />
+        <button type="submit">Search</button>
+      </Form>
+
+      <Repositories>
+        <a href="test">
+          <img
+            src="https://gitlab.com/uploads/-/system/user/avatar/5380397/avatar.png?width=400"
+            alt="foo"
+          />
+          <div>
+            <strong>franciscoescastro/bin-2-dec</strong>
+            <p>Binary to decimal converter</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+      </Repositories>
+    </>
+  )
 }
 
 export default Dashboard
